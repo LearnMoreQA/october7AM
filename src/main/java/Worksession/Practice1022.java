d427cb81c4c2f4b656b914b65c4fee6cef0d7898
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-11-02 21:34:00 -0400

    code added

---
+++ src/main/java/Worksession/Practice1022.java
@@ -0,0 +1,58 @@
+package Worksession;
+
+
+import java.util.Scanner;
+
+public class Practice1022 {
+
+    public static void main(String[] args) {
+
+//Even or odd
+       /*Scanner input = new Scanner(System.in);
+        int number;
+        System.out.println("Enter the number: ");
+        number = input.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println("The number is even");
+        } else {
+            System.out.println("The number is odd");
+        }
+
+
+        //Vowel or consonant
+        /*char ch;
+       Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the character: ");
+        ch = scan.next().charAt(0);
+
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+
+            System.out.println("ch is vowel");
+        } else {
+            System.out.println("ch  is consonant");
+        }*/
+
+
+        //Factorial
+        /*int n, fact = 1;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        n = scan.nextInt();
+
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of number " + n + " is " + fact);*/
+
+
+        //
+    }
+        }
+
+
+
+
+
---
+++ src/main/java/Worksession/Practicenestedif.java
@@ -0,0 +1,84 @@
+package Worksession;
+
+public class Practicenestedif {
+
+
+    public static void main(String[] args) {
+
+        /*for (int i = 0; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }*/
+       int i, j, rowCount = 4;
+        /*for (i = 1; i <= rowCount; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (i =1 ; i <= rowCount;i++) {
+            for (j = 4; j >= i;j--) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }*/
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
