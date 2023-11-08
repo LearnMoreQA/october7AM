c909a598f15c5d7322af714f0411f8501fa2a95e
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-11-08 18:30:11 -0500

    updated

--- src/test/java/Seleniumpractice.java
+++ src/test/java/Seleniumpractice.java
@@ -3,23 +3,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Seleniumpractice {
+WebDriver driver;
 
 
 
-        public static void main (String[]args)
-        {
-
-            WebDriver driver = new ChromeDriver();
-            driver.get("https://learnmoreplayground.blogspot.com/p/loginpage.html");
-            driver.findElement(By.id("username")).sendKeys("Learnmore");
-            driver.findElement(By.id("password")).sendKeys("learnmore@23");
-            driver.findElement(By.id("LoginBtn")).click();
+    //Login method
+    public void login()
+{
+    driver = new ChromeDriver();
+    driver.get("https://learnmoreplayground.blogspot.com/");
+    driver.findElement(By.id("username")).sendKeys("Learnmore");
+    driver.findElement(By.id("password")).sendKeys("learnmore@23");
+            /*//WebElement button = driver.findElement(By.tagName("label"));
+           // driver.findElement(By.tagName(buttononclick)).click();
             String actualText = driver.findElement(By.id("errorMessage")).getText();
             String expectedText = "Invalid Credentials.Please Check Once";
 
             if(actualText.equals(expectedText)){
                 System.out.println("TC PASS");
-            }else System.out.println("TC FAIL");
+            }else System.out.println("TC FAIL");*/
+}
+
+
+//
+
+    public void linktext()
+    {
+    driver.findElement(By.partialLinkText("Page")).click();
+    }
+        public static void main (String[]args)
+        {
+            Seleniumpractice objcall = new Seleniumpractice();
+            objcall.login();
+            objcall.linktext();
+
+
+
+
+
         }
 }
 
