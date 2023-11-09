a9b9880bb3c76617421f65e9c7ea18c1c091fac6
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-11-08 20:26:10 -0500

    updated

--- src/test/java/Seleniumpractice.java
+++ src/test/java/Seleniumpractice.java
@@ -1,48 +1,49 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Seleniumpractice {
-WebDriver driver;
+
+    WebDriver driver;
 
 
-
-    //Login method
-    public void login()
+public void login()
 {
-    driver = new ChromeDriver();
+
+     driver = new ChromeDriver();
     driver.get("https://learnmoreplayground.blogspot.com/");
+    driver.findElement(By.partialLinkText("Page")).click();
     driver.findElement(By.id("username")).sendKeys("Learnmore");
     driver.findElement(By.id("password")).sendKeys("learnmore@23");
-            /*//WebElement button = driver.findElement(By.tagName("label"));
-           // driver.findElement(By.tagName(buttononclick)).click();
-            String actualText = driver.findElement(By.id("errorMessage")).getText();
-            String expectedText = "Invalid Credentials.Please Check Once";
 
-            if(actualText.equals(expectedText)){
-                System.out.println("TC PASS");
-            }else System.out.println("TC FAIL");*/
 }
 
 
-//
-
-    public void linktext()
+    public void welcome()
     {
-    driver.findElement(By.partialLinkText("Page")).click();
+        WebElement welcome = driver.findElement(By.className("login-label"));
+        if(welcome.isDisplayed()){
+            System.out.println(welcome.getText() + " is Present");
+        }
     }
-        public static void main (String[]args)
-        {
-            Seleniumpractice objcall = new Seleniumpractice();
-            objcall.login();
-            objcall.linktext();
 
 
 
+
+    public static void main(String[]args)
+    {
+
+        Seleniumpractice objcall = new Seleniumpractice();
+        objcall.login();
+        objcall.welcome();
 
 
         }
-}
+    }
+
+
+
 
 
 
