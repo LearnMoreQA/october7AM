fb27a581abd0782031793b9fdb76029a1ff9d8bf
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-11-14 18:17:52 -0500

    Task1114added

--- src/test/java/Seleniumpractice.java
+++ src/test/java/Seleniumpractice.java
@@ -6,38 +6,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Seleniumpractice {
 
     WebDriver driver;
-
-
-public void login()
-{
+    public void login() {
 
     driver = new ChromeDriver();
     driver.get("https://learnmoreplayground.blogspot.com/");
     driver.findElement(By.partialLinkText("Page")).click();
     driver.findElement(By.id("username")).sendKeys("LearnMore");
     driver.findElement(By.id("password")).sendKeys("learnmore@123");
-    WebElement button = driver.findElement(By.tagName("button")).click()
+    WebElement button=driver.findElement(By.tagName("button"));
+    button.click();
+        }
 
-
-}
-
-
-    public void welcometextcheck() {
+        public void welcometextcheck() {
         WebElement welcome = driver.findElement(By.cssSelector(".login-label"));
         if (welcome.isDisplayed()) {
             System.out.println(welcome.getText() + " is Present");
         }
     }
-
-
-
-        public static void main (String[]args)
+    public static void main (String[]args)
         {
-
             Seleniumpractice objCall = new Seleniumpractice();
             objCall.login();
             objCall.welcometextcheck();
-
 
         }
     }
