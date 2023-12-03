1eadd6dd82f687951d1a7663dd7410c09cbcb9a4
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-12-03 18:06:47 -0500

    added

--- src/test/java/stepdefinition/PracticeStepDef.java
+++ src/test/java/stepdefinition/PracticeStepDef.java
@@ -2,22 +2,23 @@ package stepdefinition;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PracticeStepDef {
 
+
+
+
     @Given("User navigates to Learnmoreqa url")
-    public void user_navigates_to_learnmoreqa_url() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void navigatetourl() {
+        driver = new ChromeDriver();
+        driver.get("https://learnmoreplayground.blogspot.com/p/dropdown.html");
     }
     @Given("User selects Ruby from dropdown")
-    public void user_selects_ruby_from_dropdown() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void Selectdropdown() {
     }
     @Then("User verifies the dropdown selection")
-    public void user_verifies_the_dropdown_selection() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void Verifydropdown() {
+
     }
 }
