0dfcb01a103260ebe33680373bfb75021ccd122c
MathankumarGit <mechmathan16@gmail.com>
2023-12-03 18:07:25 +0530

    Validate
    Validate invalid login msg

--- src/test/java/ValidateInvalidLoginMsg.java
+++ src/test/java/ValidateInvalidLoginMsg.java
@@ -1,5 +1,3 @@
-package Tasks;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,7 +8,7 @@ public class ValidateInvalidLoginMsg {
         driver.get("https://learnmoreplayground.blogspot.com/p/loginpage.html");
         driver.findElement(By.id("username")).sendKeys("LearnMore");
         driver.findElement(By.id("password")).sendKeys("abcd123");
-        driver.findElement(By.id("LoginBtn")).click();
+        driver.findElement(By.xpath("//*[text()='Login']")).click();
         String errorMsg=driver.findElement(By.id("errorMessage")).getText();
         if (errorMsg.equals("Invalid Credentials.Please Check Once")){
             System.out.println("Test case PASS");
---
+++ target/test-classes/feature/learnMoreLogin.feature
@@ -0,0 +1,3 @@
+Feature: Automate LearnMore site
+  Scenario: To validate login with valid credentials
+    Given User Navigates to LearnMore URL
