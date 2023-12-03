33a2d54d03cd3cf80f37d1c6b579b9521dd4bd92
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-12-03 18:02:54 -0500

    added

--- src/test/java/stepdefinition/PracticeStepDef.java
+++ src/test/java/stepdefinition/PracticeStepDef.java
@@ -1,4 +1,23 @@
 package stepdefinition;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
 public class PracticeStepDef {
+
+    @Given("User navigates to Learnmoreqa url")
+    public void user_navigates_to_learnmoreqa_url() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("User selects Ruby from dropdown")
+    public void user_selects_ruby_from_dropdown() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("User verifies the dropdown selection")
+    public void user_verifies_the_dropdown_selection() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
