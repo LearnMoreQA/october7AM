7170bd3d21030d2241a68f2abb216863c1e43208
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-12-05 20:50:53 -0500

    added

--- src/test/java/testrunner/Runnerpractice.java
+++ src/test/java/testrunner/Runnerpractice.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/feature/practice.feature",
+@CucumberOptions(features = "src/test/resources/featurepractice",
         glue = "stepdefinition",tags = "@Dropdown1205")
 public class Runnerpractice {
 
