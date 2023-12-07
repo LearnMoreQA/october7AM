bd9391e7c0523c82e7f873302102ab07f339f29d
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-12-06 21:35:35 -0500

    commit

--- src/test/java/testrunner/Runner.java
+++ src/test/java/testrunner/Runner.java
@@ -5,12 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
-@CucumberOptions(features = "src/test/resources/featurepractice",
-        glue = "stepdefinition",tags = "@Dynamicdropdown")
-
 @CucumberOptions(features = "src/test/resources/feature",
         glue = "stepdefinition",tags = "@PromptAlert")
-
 public class Runner {
 }
