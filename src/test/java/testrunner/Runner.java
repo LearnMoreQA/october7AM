5054f74a1a3161839ef5f2532807abef5fae6796
Lakshmi Priya <tslakshmipriya@gmail.com>
2023-12-13 21:38:58 -0500

    Update Runner.java

--- src/test/java/testrunner/Runner.java
+++ src/test/java/testrunner/Runner.java
@@ -6,9 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/feature",
-
-        
-
         glue = "stepdefinition",tags = "@Register")
 
 public class Runner {
