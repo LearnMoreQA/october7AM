b88ee0c1e1ba712f076c7cbbfbf8efb8d8a62b31
bearerto <learnmoreqa@gmail.com>
2023-12-27 08:15:45 +0530

    Exception Handling Added

--- src/test/java/testrunner/Runner.java
+++ src/test/java/testrunner/Runner.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/feature",
-        glue = "stepdefinition",tags = "@Task")
+        glue = "stepdefinition",tags = "@Clear")
 public class Runner {
 }
---
+++ src/test/resources/feature/Bing.feature
@@ -0,0 +1,7 @@
+Feature: To Handle Exception
+
+  @Clear
+  Scenario: To validate Clear Text in Bing
+    Given user navigates to url
+    When User enters the Keyword
+    And User Clears the Keyword
